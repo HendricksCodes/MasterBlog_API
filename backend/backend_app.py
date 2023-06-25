1d9c6346d149f32f5ed975f722e5052a48291615
HendricksCodes <natejh1124@gmail.com>
2023-06-25 12:36:30 -0700

    Added delete and add post methods

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,6 +13,40 @@ POSTS = [
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    if not request.json or 'title' not in request.json or 'content' not in request.json:
+        abort(400)  # Bad request: title and content are required
+
+    title = request.json['title']
+    content = request.json['content']
+
+    # Generate new post ID
+    new_post_id = max(post['id'] for post in POSTS) + 1 if POSTS else 1
+
+    # Create new post object
+    new_post = {
+        'id': new_post_id,
+        'title': title,
+        'content': content
+    }
+
+    # append new post to list of posts
+    POSTS.append(new_post)
+
+    return jsonify(new_post), 201
+
+
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
+def delete_post(id):
+    post_index = next((index for index, post in enumerate(POSTS) if post['id'] == id), None)
+    if post_index is None:
+        abort(404)  # post with given id not found
+
+    deleted_post = POSTS.pop(post_index)
+    return jsonify({"message": f"Post with id {deleted_post['id']} has been deleted!"}), 200
 
 
 if __name__ == '__main__':
